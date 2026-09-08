c081524f6b93aa615d4b476018a6498bd784ba87
keyniciro <khendzaky44@gmail.com>
2026-09-08 14:38:29 +0700

    update filtred api

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -6,6 +6,7 @@ use App\Models\Task;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
 
 class TaskController extends Controller
 {
@@ -14,9 +15,11 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return Task::all();
+        // return Task::all();
         // $tasks = Task::all();
         // return response()->json($tasks);
+        // return TaskResource::collection(Task::all());
+        return Task::all()->toResourceCollection(TaskResource::class);
     }
 
     /**
